4d9424827c29ff70b865e8d49a856042cf2b0e08
TheProShizer <marcin.siek20@gmail.com>
2023-06-04 17:44:38 +0200

    aktualizacja

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   prefix: "!",
-  owner: "541526393641500675",
-  token: "MTExMDI2ODk2NDU4MTQyOTM2OA.GzZ-JM.l09JVn3S5iyYmEFUXb_k9KSvJ8EVR-b_RUF590",
+  owner: "TWOJE-ID",
+  token: "TWÓJ-TOKEN",
 }
